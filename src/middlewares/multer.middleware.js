7f9bcc6b11b1e382612272db44f78603ec3641c3
Suraj-1309 <surajsinghch2055@gmail.com>
2026-03-23 19:53:20 +0530

    work on file uploading utility

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,19 @@
+import multer from "multer";
+
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp")
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.origianlname)
+  }
+})
+
+export const upload = multer({ 
+    storage: storage 
+})
+
+// req -> body of json data
+// file -> file uploaded on server
+// cb -> callback
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import { v2 as cloudinary} from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUTINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUTINARY_API_KEY, 
+  api_secret: process.env.CLOUTINARY_API_SECERT,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try{
+        if(!localFilePath) return null;
+        // upload the file on cloudinary
+        const response = await cloudinary.uploader.upload
+        (localFilePath, {
+            resource_type: "auto"
+        })
+
+        // file has been uploaded successfull
+        console.log("file is uploaded on cloudinary ",
+            response.url
+        );
+        return response;
+    }catch(error){
+        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload opeartion got failed
+        return null;
+    }
+}
+
+export {uploadOnCloudinary}
