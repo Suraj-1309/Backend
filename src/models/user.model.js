7bb6cb80e9be4616775334b52cd12d4610f64266
Suraj-1309 <surajsinghch2055@gmail.com>
2026-03-26 10:05:13 +0530

    add register controller and bug fixes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -54,13 +54,15 @@ const userSchema = new Schema(
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
 
-  this.password = bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
   next();
 });
+
 
 userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
+
 
 userSchema.methods.generateAccessToken = function () {
   return jwt.sign(
@@ -76,6 +78,7 @@ userSchema.methods.generateAccessToken = function () {
     }
   );
 };
+
 
 userSchema.methods.generateRefreshToken = async function () {
   return jwt.sign(
