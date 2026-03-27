fb078477cbba12f35c2bddbd6dd0c0605f9b8095
Suraj-1309 <surajsinghch2055@gmail.com>
2026-03-27 11:43:20 +0530

    fix a small bug

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -92,4 +92,4 @@ userSchema.methods.generateRefreshToken = async function () {
   );
 };
 
-export default User = mongoose.model("User", userSchema);
+export const User = mongoose.model("User", userSchema);
