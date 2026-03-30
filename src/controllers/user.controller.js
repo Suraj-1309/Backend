0313cc1ee919ba80fd151aca386930f8371b10d8
Suraj-1309 <surajsinghch2055@gmail.com>
2026-03-30 10:40:21 +0530

    add subscription model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -190,10 +190,10 @@ const logoutUser = asyncHandler(async (req, res) => {
 const refreshAccessToken = asyncHandler(async (req, res) => {
     const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken;
 
-    if (incomingRefreshToken) {
+    if (!incomingRefreshToken) {
         throw new ApiError(401, "unauthorized request");
     }
-
+    
     try {
         const decodedToken = jwt.verify(
             incomingRefreshToken,
